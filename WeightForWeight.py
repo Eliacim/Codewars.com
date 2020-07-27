8ef9622daddb0c283009f7339f117e5e18df8293
Eliacim <eliacim@eliacim.com.br>
2020-07-26 19:39:29 -0500

    DONE

--- WeightForWeight.py
+++ WeightForWeight.py
@@ -44,8 +44,17 @@ def order_weight2(strng):
                     strng.split()], key=lambda x: [x[1], x[0]]))
 
 
+# This is one of the best practices/clever in codewars:
+def order_weight3(_str):
+    return ' '.join(sorted(sorted(_str.split(' ')),
+                    key=lambda x: sum(int(c) for c in x)))
+
+
 print(order_weight("103 123 4444 99 2000"))
 # "2000 103 123 4444 99"
 
 print(order_weight2("2000 10003 1234000 44444444 9999 11 11 22 123"))
+# "11 11 2000 10003 22 123 1234000 44444444 9999"
+
+print(order_weight3("2000 10003 1234000 44444444 9999 11 11 22 123"))
 # "11 11 2000 10003 22 123 1234000 44444444 9999"
