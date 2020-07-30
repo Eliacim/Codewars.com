4f8980ab45faaa8efe54b643ac6e8d8b3a3228fc
Eliacim <eliacim@eliacim.com.br>
2020-07-29 22:09:31 -0500

    Done

--- IntegersRecreationOne.py
+++ IntegersRecreationOne.py
@@ -19,18 +19,54 @@ list_squared(1, 250) --> [[1, 1], [42, 2500], [246, 84100]] list_squared(42,
 '''
 
 
-import math
+from math import sqrt, ceil
 
 
-def list_squared(m, n):
+# Works, but is too slow to codewars
+def list_squared1(m, n):
+    if m >= 1 and m <= n:
+        divisors = []
+        for mn in range(m, n + 1):
+            tmp = sum([num ** 2 for num in range(1, mn + 1) if mn % num == 0])
+            if float.is_integer(sqrt(tmp)) or tmp == 1:
+                divisors.append([mn, tmp])
+        return(divisors)
+
+
+# Works + execution time acceptable by codewars
+def list_squared2(m, n):
     if m >= 1 and m <= n:
         divisors = []
         for mn in range(m, n):
-            tmp = [num for num in range(1, mn) if mn % num == 0]
-            tmp = sum([t ** 2 for t in tmp])
-            print("\n", tmp, math.sqrt(tmp))
-        
+            sumsqrt = 0
+            for num in range(1, ceil(sqrt(mn))):  # listing till sqrt!
+                if mn % num == 0:
+                    sumsqrt += num ** 2
+                    if not mn == 1:
+                        sumsqrt += (mn // num) ** 2
+            if mn == 1:
+                sumsqrt = 1
+            if float.is_integer(sqrt(sumsqrt)):
+                divisors.append([mn, sumsqrt])
+        return divisors
 
+
+# This is the tempative to sumarized the code above
+def list_squared(m, n):
+    if m >= 1 and m <= n:
+        divisors = []
+
+        for mn in range(m, n):
+
+            tmp = sum([num ** 2 + ((mn // num) ** 2 if not mn == 1 else 0)
+                      for num in range(1, ceil(sqrt(mn))) if mn % num == 0])
+
+            tmp = 1 if mn == 1 else tmp
+
+            if float.is_integer(sqrt(tmp)):
+                divisors.append([mn, tmp])
+
+        return divisors
 
 
 print(list_squared(1, 250))  # [[1, 1], [42, 2500], [246, 84100]])
