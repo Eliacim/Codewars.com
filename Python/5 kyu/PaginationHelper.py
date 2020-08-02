2cd33ad48ae66209d9216abccce04e6a6ad45e94
Eliacim <eliacim@eliacim.com.br>
2020-08-01 22:25:26 -0500

    done

---
+++ Python/5 kyu/PaginationHelper.py
@@ -0,0 +1,63 @@
+'''
+https://www.codewars.com/kata/515bb423de843ea99400000a/train/python
+
+5 kyu PaginationHelper
+
+For this exercise you will be strengthening your page-fu mastery. You will
+complete the PaginationHelper class, which is a utility class helpful for
+querying paging information related to an array.
+
+The class is designed to take in an array of values and an integer indicating
+how many items will be allowed per each page. The types of values contained
+within the collection/array are not relevant.
+
+The following are some examples of how this class is used:
+
+helper = PaginationHelper(['a','b','c','d','e','f'], 4)
+helper.page_count # should == 2
+helper.item_count # should == 6
+helper.page_item_count(0)  # should == 4
+helper.page_item_count(1) # last page - should == 2
+helper.page_item_count(2) # should == -1 since the page is invalid
+
+# page_index takes an item index and returns the page that it belongs on
+helper.page_index(5) # should == 1 (zero based index)
+helper.page_index(2) # should == 0
+helper.page_index(20) # should == -1
+helper.page_index(-10) # should == -1 because negative indexes are invalid
+'''
+
+
+class PaginationHelper:
+
+    def __init__(self, collection, items_per_page):
+        self.collection = collection
+        self.items_per_page = items_per_page
+
+    def item_count(self):
+        return len(self.collection)
+
+    def page_count(self):
+        return int(self.item_count() / self.items_per_page + 1)
+
+    def page_item_count(self, page_index):
+        if page_index >= self.page_count():
+            return -1
+        elif page_index == self.page_count() - 1:
+            return len(self.collection) % self.items_per_page or\
+                self.items_per_page
+        else:
+            return self.items_per_page
+
+    def page_index(self, item_index):
+        return int(item_index / self.items_per_page)\
+            if item_index >= 0 and item_index < self.item_count() else -1
+
+
+collection = range(1, 25)
+helper = PaginationHelper(collection, 10)
+
+print(helper.item_count())  # 24
+print(helper.page_count())  # 3
+print(helper.page_index(23))  # 2
+print(helper.page_item_count(23))
