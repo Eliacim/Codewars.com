4e4e10985b6b76bffe7be530b58bef95cb2c2d41
Eliacim <eliacim@eliacim.com.br>
2020-08-04 22:26:52 -0500

    Done

---
+++ Python/8 kyu/OppositeNumber.py
@@ -0,0 +1,22 @@
+'''
+https://www.codewars.com/kata/56dec885c54a926dcd001095/python
+
+8 kyu
+Opposite number
+
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+'''
+
+
+def opposite(number):
+    return -number
+
+
+print(opposite(1))  # -1)
+print(opposite(-34))  # 34)
